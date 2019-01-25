e64d7e324c2f01b27c2989f80f61a84b3a023278
TuckerDS <jjperez.tucker.dev@gmail.com>
2019-01-25 10:51:15 +0100

    init commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var Server = require('ws').Server;
+var port = process.env.PORT || 3001;
+var ws = new Server({ port: port });
+var clients = [];
+
+ws.on('connection', function (w, req) {
+  var id = Date.now();
+
+  console.log("NEW CONN:", id)
+  clients.push({
+    id: id,
+    con: w
+  })
+  
+  w.on('message', function (msg) {
+    console.log('message from client', JSON.parse(msg));
+    sendBroadcast(msg)
+  });
+
+  w.on('close', function () {
+    console.log('CLOSE CONN', id);
+    removeClient(id)
+  });
+  w.send(JSON.stringify({id:id, type:"HELO"}));
+});
+
+var removeClient = function (id) {
+  var newArray = []
+  for (i = 0; i < clients.length; i++) {
+    if (clients[i].id != id) {
+      newArray.push(clients[i])
+    }
+  }
+  clients = newArray
+}
+
+var sendBroadcast = function (msg) {
+  for (i = 0; i < clients.length; i++) {
+    clients[i].con.send(msg)
+  }
+}
