5182f7c19ef4556870de5b1df199cbe0bf731f5f
TuckerDS <jjperez.tucker.dev@gmail.com>
2019-01-30 23:09:44 +0100

    es6

--- server.js
+++ server.js
@@ -7,19 +7,19 @@ var clients = [];
 ws.on('connection', function (w, req) {
   var id = Date.now();
   clients.push({ 'id': id, 'con': w, 'nick': 'Anon'});
-  console.log("NEW CONN:", id)
+  console.log("NEW CONN:", id);
   
   w.on('message', function (msg) {
     var parsedMsg = JSON.parse(msg);
     console.log('NEW MSG:', parsedMsg);
     switch (parsedMsg.type) {
       case 'NICK':
-        clients.find(e => e.id == id).nick = parsedMsg.nick
-        sendMsgs(clients, {type: "LIST", content: clients.map(e => { return {id: e.id, nick: e.nick} })})
+        clients.find(e => e.id == id).nick = parsedMsg.nick;
+        sendMsgs(clients, {type: "LIST", content: clients.map(e => { return {id: e.id, nick: e.nick}; })});
         break;
       case 'CHAT':
-        var client = clients.find(e => e.id == parsedMsg.to)
-        sendMsgs([client], parsedMsg)
+        var client = clients.find(e => e.id == parsedMsg.to);
+        sendMsgs([client], parsedMsg);
         break;
       case 'TEXT':
         sendMsgs(clients, parsedMsg);
@@ -30,21 +30,21 @@ ws.on('connection', function (w, req) {
 
   w.on('close', function () {
     console.log('CLOSE CONN:', id);
-    removeClient(id)
+    removeClient(id);
   });
 
   w.send(JSON.stringify({id:id, type:"HELO"}));
-  sendMsgs(clients, {type: "LIST", content: clients.map(e => {return {id: e.id, nick: e.nick}})})
+  sendMsgs(clients, {type: "LIST", content: clients.map(e => {return {id: e.id, nick: e.nick};})});
 });
 
 var removeClient = function (id) {
-  var newArray = clients.filter(e => e.id != id)
-  clients = newArray
-  sendMsgs(clients, {type: "LIST", content: clients.map(e =>{return {id: e.id, nick: e.nick}})})
+  var newArray = clients.filter(e => e.id != id);
+  clients = newArray;
+  sendMsgs(clients, {type: "LIST", content: clients.map(e =>{return {id: e.id, nick: e.nick};})});
 }
 
 var sendMsgs = function (recipients, msg) {
   for (i = 0; i < recipients.length; i++) {
-    try {recipients[i].con.send(JSON.stringify(msg))} catch (e) {console.log('Error:', e)}
+    try {recipients[i].con.send(JSON.stringify(msg));} catch (e) {console.log('Error:', e);}
   }
-}
+};
